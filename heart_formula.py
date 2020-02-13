4903386b6bff4b8e0ac8fbdd9af797bcb5d3e400
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2020-02-13 23:09:20 +0100

    Update heart_formula.py

--- heart_formula.py
+++ heart_formula.py
@@ -7,11 +7,11 @@ def heart_1(x):
 def heart_2(x):
     return -3*sqrt(1-(abs(x)/2)**0.5)
     
-x = []
+
+x = np.linspace(-2,2,1000)
 y1 = []
 y2 = []
-for i in np.linspace(-2,2,1000):
-    x.append(i)
+for i in x :
     y1.append(heart_1(i))
     y2.append(heart_2(i))
     
