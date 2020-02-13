a9f8b85ac0da931016af9ad088f8af362b7ded0f
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2020-02-13 23:05:32 +0100

    Create heart_formula.py

---
+++ heart_formula.py
@@ -0,0 +1,20 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def heart_1(x):
+    return sqrt(1-(abs(x)-1)**2)
+def heart_2(x):
+    return -3*sqrt(1-(abs(x)/2)**0.5)
+    
+x = []
+y1 = []
+y2 = []
+for i in np.linspace(-2,2,1000):
+    x.append(i)
+    y1.append(heart_1(i))
+    y2.append(heart_2(i))
+    
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.show()
