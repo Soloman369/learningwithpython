a70fa214b7682efe832cf7018601e314c0d6b46e
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2020-02-13 23:16:01 +0100

    Update heart_formula.py

--- heart_formula.py
+++ heart_formula.py
@@ -6,15 +6,11 @@ def heart_1(x):
     return sqrt(1-(abs(x)-1)**2)
 def heart_2(x):
     return -3*sqrt(1-(abs(x)/2)**0.5)
-    
 
 x = np.linspace(-2,2,1000)
-y1 = []
-y2 = []
-for i in x :
-    y1.append(heart_1(i))
-    y2.append(heart_2(i))
-    
-plt.plot(x,y1)
-plt.plot(x,y2)
+y1 = np.vectorize(heart_1)
+y2 = np.vectorize(heart_2)
+
+plt.plot(x,y1(x))
+plt.plot(x,y2(x))
 plt.show()
